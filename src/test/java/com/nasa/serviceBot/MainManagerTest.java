f38e94ada0a01a762dd5cb33f1ccbf8f6f64dabd
Viacheslav Kucheruk <comandantes7s@gmail.com>
2025-02-26 23:37:37 +0200

    MainManager tests ended

--- src/test/java/com/nasa/serviceBot/MainManagerTest.java
+++ src/test/java/com/nasa/serviceBot/MainManagerTest.java
@@ -9,6 +9,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.methods.send.SendVideo;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -99,10 +101,54 @@ class MainManagerTest {
     }
 
     @Test
-    void sendPhoto() {
+    void testSendPhoto() throws TelegramApiException {
+        String URL = "https://hormigas-en-la-casa";
+        ArgumentCaptor<SendPhoto> captor = ArgumentCaptor.forClass(SendPhoto.class);
+
+        mainManager.sendPhoto(chatId, URL, message, nasaBot);
+
+        verify(nasaBot).execute(captor.capture());
+
+        SendPhoto captorValue = captor.getValue();
+
+        assertEquals(chatId.toString(), captorValue.getChatId());
+        assertNotNull(captorValue.getPhoto());
+        assertEquals(message, captorValue.getCaption());
+        assertEquals(URL, captorValue.getFile().getAttachName());
     }
 
     @Test
-    void sendVideo() {
+    void shouldSendPreviewAndKeyboardIfYoutubeVideo() throws TelegramApiException {
+        String URL = "https://youtube.com/hormigas-en-la-casa?";
+        ArgumentCaptor<SendPhoto> captor = ArgumentCaptor.forClass(SendPhoto.class);
+
+        mainManager.sendVideo(chatId, URL, message, nasaBot);
+
+        verify(nasaBot).execute(captor.capture());
+
+        SendPhoto captorValue = captor.getValue();
+
+        assertEquals(chatId.toString(), captorValue.getChatId());
+        assertNotNull(captorValue.getPhoto());
+        assertTrue(captorValue.getFile().getAttachName().contains("/hqdefault.jpg"));
+        assertEquals(message, captorValue.getCaption());
+        assertNotNull(captorValue.getReplyMarkup());
+    }
+
+    @Test
+    void shouldSendVideoIfDontContainsYoutubeVideo() throws TelegramApiException {
+        String URL = "https://hormigas-en-la-casa?";
+        ArgumentCaptor<SendVideo> captor = ArgumentCaptor.forClass(SendVideo.class);
+
+        mainManager.sendVideo(chatId, URL, message, nasaBot);
+
+        verify(nasaBot).execute(captor.capture());
+
+        SendVideo captorValue = captor.getValue();
+
+        assertEquals(chatId.toString(), captorValue.getChatId());
+        assertNotNull(captorValue.getVideo());
+        assertTrue(captorValue.getFile().getAttachName().equalsIgnoreCase(URL));
+        assertEquals(message, captorValue.getCaption());
     }
 }
---
+++ src/test/java/com/nasa/serviceNasaAPI/impl/AstroInfoTest.java
@@ -0,0 +1,12 @@
+package com.nasa.serviceNasaAPI.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AstroInfoTest {
+
+    @Test
+    void constructResponse() {
+    }
+}
